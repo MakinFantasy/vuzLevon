d6ff0b95ee49aca5aa638227dfb668acc1cdb3c2
MakinFantasy <beingp0z1t1v3@gmail.com>
2022-12-12 00:17:36 +0300

    laba2 finished

--- laba2/task1.py
+++ laba2/task1.py
@@ -4,11 +4,17 @@ from math import *
 try:
     x = float(input('Введите x: '))
     if x <= -2.5:
-        y = -6/7*x - 36/7
-    elif -2.5 < x <= 2:
-        y = 0.10101010
+        y = -6 / 7 * x - 36 / 7
+    elif -2.5 < x <= -1:
+        y = -4 * x ** 2 - 12 * x - 8
+    elif -1 < x <= 0:
+        y = -3 * x - 3
+    elif 0 < x <= 1.5:
+        y = 4 * x ** 2 - 4 * x - 3
+    elif 1.5 < x <= 2:
+        y = 12 * x - 18
     elif 2 < x:
-        y = -2*x + 10
+        y = -2 * x + 10
 except Exception as e:
     print(e)
     print(e.__doc__)
