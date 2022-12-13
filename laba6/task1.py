c9e90ff9a95cfbc215dc045663be7fd5465e5b45
MakinFantasy <beingp0z1t1v3@gmail.com>
2022-12-14 01:23:47 +0300

    fixed previous

--- laba6/task1.py
+++ laba6/task1.py
@@ -39,16 +39,21 @@ def main():
             except Exception as e:
                 res.write("Тест" + str(i + 1) + ": " + "a=" + str(a) + " " + "b=" + str(b) + ": " + "ОДЗ" + '\n')
 
-        # print("Вторая формула: ", '\n')
-        # for j in range(5):
-        #     print('Тест ', j + 1)
-        #     a = int(input("Введите параметр а: "))
-        #     b = int(input("Введите параметр b: "))
-        #     try:
-        #         z = tan(a * b + (3 / 2) * pi)
-        #         print("Ответ: ", z)
-        #     except Exception as e:
-        #         print("Ответ: ОДЗ")
+        res.write("Вторая формула: " + "\n")
+        arr = file.readline().split(' ')
+        values = []
+        for el in arr:
+            values.append(int(el))
+        for i in range(len(values) - 1):
+            print('Тест ', i + 1)
+            a = values[i]
+            b = values[i + 1]
+            try:
+                z = a ** sqrt(log(b, a)) - b ** sqrt(log(a, b)) + tan(a * b + 3 * pi / 2)
+                res.write("Тест" + str(i + 1) + ": " + "a=" + str(a) + " " + "b=" + str(b) + ": " + str(z) + '\n')
+            except Exception as e:
+                res.write("Тест" + str(i + 1) + ": " + "a=" + str(a) + " " + "b=" + str(b) + ": " + "ОДЗ" + '\n')
+
     except Exception as e:
         print(e)
         print(e.__doc__)
