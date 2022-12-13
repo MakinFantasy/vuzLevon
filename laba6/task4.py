888313c7e5093e9b50861a9225f54d06e649a5ae
MakinFantasy <beingp0z1t1v3@gmail.com>
2022-12-14 01:33:53 +0300

    laba task2 finished

--- laba6/task4.py
+++ laba6/task4.py
@@ -21,14 +21,13 @@ def clear_line():
         print(e.__doc__)
         exit(1)
 
+
 def init():
     try:
-        n = int(input('Введите кол-во элементов: '))
-        array = []
-        for i in range(n):
-            array.append(randint(-5, 5))
-        print(array)
-        return array
+        line = file.readline().split(' ')
+        values = [int(el) for el in line]
+        print(values)
+        return values
     except Exception as e:
         print(e)
         print(e.__doc__)
@@ -39,24 +38,24 @@ def less_y_sum(y, array):
     new_arr = [i for i in array if abs(i) < y]
     to_return = [i for i in array if abs(i) >= y]
     result = sum(new_arr)
-    # print(new_arr)
-    print('Искомая сумма: ', result)
+    res.write('Сумма: ' + str(result))
     return to_return
 
 
 def multiplication(array):
-    # print(array)
     result = 1
     for i in array:
         result *= i
-    print('Искомое произведение: ', result)
+    res.write('Произведение: ' + str(result))
     return result
 
 
 def main():
+    clear_file()
     arr = init()
     y = float(input('Введите значение y: '))
     sum = less_y_sum(y, arr)
+    clear_line()
     multiplication(sum)
 
 main()
