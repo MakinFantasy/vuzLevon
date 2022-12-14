84fcd64926ef4fe0836d3c02ed7f5cf8d9eb10e8
MakinFantasy <beingp0z1t1v3@gmail.com>
2022-12-14 22:36:26 +0300

    finished

--- laba2/task2.py
+++ laba2/task2.py
@@ -8,7 +8,6 @@ except Exception as e:
     exit(1)
 
 try:
-    print('Start')
     if ((0 <= X <= R) and (0 <= Y <= R) and (R**2 <= X**2 + Y**2)) or ((R**2 >= X**2 + Y**2) and (Y >= X + R)) \
             or ((-R <= X <= 0) and (-R <= Y <= 0) and (R**2 <= X**2 + Y**2)) or ((R**2 >= X**2 + Y**2) and (Y <= X - R)):
         print('Попал!')
